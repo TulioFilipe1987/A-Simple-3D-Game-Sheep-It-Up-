3da0c9159c811527be195367753d47def81bc404
TulioFilipe1987 <tulio.ruy@gmail.com>
2022-08-27 14:36:10 -0300

    Game of sheep

---
+++ Assets/Scripts/Level Scripts/LevelGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject startPlatform, endPlatform, platformPrefab;
+
+    private float blockWidth = 0.5f, blockHeight = 0.2f;
+
+    [SerializeField]
+    private int amountToSpawn = 100;
+    private int beginAmount = 0; // inicio da quantia 
+
+    private Vector3 lastPos;
+
+    private List<GameObject> spawnedPlataforms = new List<GameObject>();
+
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    void Awake()
+    {
+        InstantiateLevel();
+    }
+     
+
+    void InstantiateLevel(){
+
+        for(int i= beginAmount; i< amountToSpawn; i++){
+
+            GameObject newPlatform;
+
+            if(i == 0){
+
+                newPlatform = Instantiate(startPlatform);
+
+            }else if( i == amountToSpawn - 1){// 99
+
+                newPlatform = Instantiate(endPlatform);
+                newPlatform.tag = "EndPlatform";
+            
+            }else{
+
+                newPlatform = Instantiate(platformPrefab);
+
+            }
+
+            newPlatform.transform.parent = transform; //
+
+
+            spawnedPlataforms.Add(newPlatform);
+
+
+            // other "IF" ------------
+
+            if(i == 0){
+
+                lastPos = newPlatform.transform.position;
+
+                // instatiate the player
+                Vector3 temp = lastPos;
+                temp.y += 0.1f;
+                Instantiate(playerPrefab, temp, Quaternion.identity);
+ 
+                continue;
+
+            }
+
+            int left = Random.Range(0, 2);
+
+            if(left == 0){
+                newPlatform.transform.position =
+                    new Vector3(lastPos.x - blockWidth, lastPos.y + blockHeight, lastPos.z);
+
+            }
+            else{
+
+                newPlatform.transform.position =
+                    new Vector3(lastPos.x , lastPos.y + blockHeight, lastPos.z + blockWidth);
+
+
+            }
+
+            lastPos = newPlatform.transform.position;
+
+            // fancy animation - animacao estravagante 
+            
+            if(i < 25){
+
+                float endPos = newPlatform.transform.position.y;
+
+                newPlatform.transform.position =
+                    new Vector3(newPlatform.transform.position.x,
+                    newPlatform.transform.position.y - blockHeight * 3f,
+                    newPlatform.transform.position.z);
+
+                newPlatform.transform.DOLocalMoveY(endPos, 0.3f).SetDelay(i * 0.1f);
+            
+            }
+
+        }// for loop
+
+
+    } // Instantiate level
+     
+}// class
---
+++ Assets/Scripts/Player Script/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float movementForce = 0.5f, jumpForce = 0.15f;
+    private float jumpTime = 0.15f;
+
+    void Awake(){
+
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update(){
+
+        GetInput();
+    }
+
+    void GetInput(){
+     
+         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+
+
+            Jump(true);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+
+            Jump(false);
+        
+        }
+
+    }
+
+    void Jump(bool left){
+
+        // SoundManager.instance.jumpSound();
+
+        if (left){
+
+            transform.DORotate(new Vector3(0f, 90f, 0f), 0f);// x,y,z,duration
+
+            rb.DOJump(new Vector3(
+                 transform.position.x - movementForce, transform.position.y + jumpForce,
+                 transform.position.z), 0.5f ,1 ,jumpTime);// force/quatidade/time
+
+        }else{//(right)
+
+            transform.DORotate(new Vector3(0f, -180f, 0f), 0f); //(a ,b ,c(d)) this is : postion + duratio/
+
+            rb.DOJump(new Vector3(
+                 transform.position.x , transform.position.y + jumpForce,
+                 transform.position.z + movementForce), 0.5f, 1, jumpTime);
+
+
+        }
+    }
+
+
+}// class
